71c77c07bf97c6bbc8f0feb1665d720e531ee2b7
Drup <drupyog@zoho.com>
2014-01-09 14:52:30 +0100

    Minimal interface for eval, preparation for strict version of Eval.

--- src/Heap.hs
+++ src/Heap.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE RecordWildCards, GADTs, OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards, GADTs, OverloadedStrings, TypeSynonymInstances, FlexibleInstances  #-}
 
 module Heap where
 
@@ -71,3 +71,99 @@ lookHeapC x = do
   case lk of
     Nothing -> terr $ "Construction not found: " <> pretty x
     Just c -> return c
+
+addFin :: Monoid a => Id -> String -> TC a -> TC a
+addFin x t k = do
+  h <- ask
+  case M.lookup x (heapTags h) of
+    Just t' | t /= t' -> return mempty -- conflicting tags, abort.
+    _ -> local (\h' -> h' {heapTags = M.insert x t (heapTags h')}) k
+
+addDestr :: Hyp Id -> Destr Id -> TC a -> TC a
+addDestr x (Cut c _ct) k = local (addCut' x $ Right c) k
+addDestr x d k = do
+  h <- ask
+  let d' = getAlias (heapAlias h) <$> d
+  tell ["Adding destr. " <> pretty x <> " = " <> pretty d  <> " ; aliased to " <> pretty d']
+  local (addCut' x $ Left d') $ case M.lookup d' (heapDestr h) of
+     Just y -> addAlias y x k
+     Nothing -> local (addDestr' d' x) k
+
+-- | return true if fizzled, otherwise call the continuation.
+addConstr :: Monoid a => Conc Id -> Constr' -> TC a -> TC a
+addConstr x c k = do
+  tell ["Adding construction " <> pretty x <> " = " <> pretty c]
+  hC <- heapConstr <$> ask
+  hA <- heapAlias <$> ask
+  case c of
+    Tag t | Just (Tag t') <- M.lookup x hC -> if t /= t' then return mempty else k
+    _ -> local (addConstr' x $ getAlias hA <$> c) k
+
+instance Monoid Bool where
+  mempty = True
+  mappend = (&&)
+
+-- | Pretty printing
+
+class Prettier a where
+  prettier :: a -> TC Doc
+
+pConc :: Conc Id -> TC Doc
+pConc x = prettier =<< lookHeapC x
+
+pHyp :: Hyp Id -> TC Doc
+pHyp x = do
+  h <- ask
+  let ts = heapTags h
+  case M.lookup x ts of
+     Just tag -> return $ "'" <> text tag
+     _ -> do
+       let lk = M.lookup (getAlias (heapAlias h) x) $ heapCuts h
+       case lk of
+         Nothing -> return $ pretty x
+         Just (Right c) -> pConc c
+         Just (Left d) -> prettier d
+
+instance Prettier Term' where
+  prettier (Conc c) = pConc c
+  prettier (Destr h d t) = addDestr h d $ prettier t
+  prettier (Constr x c t) = addConstr x c $ prettier t
+  prettier (Case x bs) = do
+    bs' <- mapM prettier bs
+    h <- pHyp x
+    return $ hang ("case " <> h <> " of") 2 (braces $ sep $ punctuate "." $ bs')
+
+instance Prettier Constr' where
+  prettier (Hyp h) = pHyp h
+  prettier (Lam x b) = do
+    b' <- prettier b
+    return $ "\\" <> pretty x <> " -> " <> b'
+  prettier (Pi x t b) = do
+    t' <- pConc t
+    b' <- prettier b
+    return $ parens (pretty x <>":"<> t') <> " -> " <> b'
+  prettier (Sigma x t b) = do
+    t' <- pConc  t
+    b' <- prettier b
+    return $ parens (pretty x <>":"<> t') <> " × " <> b'
+  prettier (Pair a b) = do
+    a' <- pConc  a
+    b' <- pConc  b
+    return $ parens $ a' <> "," <> b'
+  prettier x = return $ pretty x
+
+instance Prettier Destr' where
+  prettier (App f x) = do
+    f' <- pHyp f
+    x' <- pConc x
+    return $ f' <+> x'
+  prettier (Proj x p) = do
+    x' <- pHyp x
+    return $ x' <> pretty p
+  prettier (Cut x t) = do
+    x' <- pConc x
+    t' <- pConc t
+    return $ x' <+> ":" <+> t'
+
+instance Prettier Branch' where
+  prettier (Br tag t) = (\x -> "'" <> text tag <> "->" <> x) <$> prettier t
